62cafabcb80b475c4197e4e72991d143dc87ec40
Adegbuyi Daniel <adegbuyidaniel42@gmail.com>
2023-07-14 01:32:16 +0100

    Update

---
+++ main.js
@@ -0,0 +1,48 @@
+var account = {
+    balance: 0
+};
+// check amount is a number
+function checkAmount (amount) {
+    if (typeof amount === "number"){
+        return true;
+    } else {
+        return false;
+    }
+}
+// Deposits amount to account
+function deposit(accountName, amount) {
+    var valid = checkAmount(amount);
+    if (valid) {
+        var deposited = accountName.balance += amount;
+        console.log("You have deposited $" + amount + ". Your account balance is $" + deposited +".");
+        return 
+    } else {
+        console.log("Please enter a number to deposit");                   
+    }
+}
+
+// Withdraws amount from account
+function withdraw(accountName, amount) {
+    var valid = checkAmount(amount);
+    if (valid) {
+        if (amount > accountName.balance){
+            console.log("I am sorry, you do not have enough funds");
+        } else {
+            return accountName.balance -= amount;
+        }
+    } else {
+        console.log("Please enter a number to deposit");        
+    }
+}
+
+// Gets account balance
+function getBalance(accountName) {
+    console.log("Your account currently has $" + account.balance + " balance.");
+}
+
+deposit(account, 35);
+deposit(account, "Fred");
+withdraw(account, "fred");
+withdraw(account, 21);
+
+getBalance(account);
